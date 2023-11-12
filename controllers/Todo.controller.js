d41d14d96b5efea5d733be63f623f34ce5515f11
Dionisius951 <dionisius951@gmail.com>
2023-11-13 03:27:24 +0700

    menambah fungsi get todos by id

--- controllers/Todo.controller.js
+++ controllers/Todo.controller.js
@@ -8,11 +8,16 @@ module.exports = {
     })},
 
     AddTodo : (req,res) => {
-        
     },
 
     getTodoByID : (req,res) => {
+        const {id} = req.params
+        const todo = Todos.find(todo => todo.id == id )
 
+        res.status(200).json({
+            message : "Berhasil mendapat data todos",
+            todos : todo
+        })
     },
 
     EditTodoByIDD : (req,res) => {
--- routes/Todo.route.js
+++ routes/Todo.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getAllTodo } = require('../controllers/Todo.controller');
+const { getAllTodo, getTodoByID } = require('../controllers/Todo.controller');
 const route = express.Router();
 
 
 route.get('/', getAllTodo)
-// route.get('/:id', sebuah fungsi)
+route.get('/:id', getTodoByID)
 // route.post('/', sebuah fungsi)
 // route.put('/:id', sebuah fungsi)
 // route.delete('/:id', sebuah fungsi)
